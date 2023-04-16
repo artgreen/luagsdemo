10415fbea0a4c44f6ab8cbff8dd8cdb9b7d54de9
green <25813+artgreen@users.noreply.github.com>
2023-04-16 18:22:59 -0500

    link with lvm.a

--- main.c
+++ main.c
@@ -18,6 +18,19 @@ int main(int argc, char *argv[]) {
     printf("Opening libs\n");
     luaL_openlibs(L);
 
+//    // Load the test_iface library
+//    printf("Loading test_iface library\n");
+//    luaL_requiref(L, "test_iface", luaopen_test_iface, 1);
+//    lua_pop(L, 1);
+//
+//    // Load and execute the LUA script
+//    printf("Executing bridge.lua\n");
+//    if (luaL_loadfile(L, "bridge.lua") || lua_pcall(L, 0, 0, 0)) {
+//        fprintf(stderr, "Error running script: %s\n", lua_tostring(L, -1));
+//        lua_close(L);
+//        return 1;
+//    }
+
     // Close the LUA state
     printf("Closing LUA state... Byeeeeeeeeeee!\n");
     lua_close(L);
