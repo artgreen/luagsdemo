fa49009cff30f7398abe75e817160668688de2b8
green <25813+artgreen@users.noreply.github.com>
2023-04-16 18:46:07 -0500

    load the module

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #pragma optimize    9
 #pragma lint       -1
 #pragma debug       0
-
 #pragma path "include"
 
 #include <stdio.h>
@@ -10,6 +9,9 @@
 #include "lua.h"
 #include "lauxlib.h"
 #include "lualib.h"
+#include "collection.h"
+
+int luaopen_collection(lua_State *L);
 
 int main(int argc, char *argv[]) {
     // Initialize the LUA state
@@ -18,11 +20,11 @@ int main(int argc, char *argv[]) {
     printf("Opening libs\n");
     luaL_openlibs(L);
 
-//    // Load the test_iface library
-//    printf("Loading test_iface library\n");
-//    luaL_requiref(L, "test_iface", luaopen_test_iface, 1);
-//    lua_pop(L, 1);
-//
+    // Load the LUA Collection library
+    printf("Loading Collection library\n");
+    luaL_requiref(L, "collection", luaopen_collection, 1);
+    lua_pop(L, 1);
+
 //    // Load and execute the LUA script
 //    printf("Executing bridge.lua\n");
 //    if (luaL_loadfile(L, "bridge.lua") || lua_pcall(L, 0, 0, 0)) {
