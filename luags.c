7735d87ac323460ed2e40a46a4845adc2a1c88f5
green <25813+artgreen@users.noreply.github.com>
2023-04-19 19:25:31 -0500

    added comments

--- luags.c
+++ luags.c
@@ -13,10 +13,6 @@
 #include "lualib.h"
 #include "lauxlib.h"
 #include "luags.h"
-#include "luafuncs.h"
-#include "status.h"
-#include "collection.h"
-
 
 /*
  * Lua state
@@ -44,7 +40,7 @@ int lg_open(void) {
 /*
  * Close the Lua library
  * No parameters
- * Returns: Nothing.
+ * Returns: nothing.
  */
 void lg_close(void) {
     // Close the LUA state
@@ -57,7 +53,7 @@ lua_State *lg_state(void) {
 /*
  * Execute a Lua script file
  * file_name: Name of a file containing Lua script or Lua bytecode
- * Result: NULL on success, on failure returns the Lua error message
+ * Returns: NULL on success, on failure returns the Lua error message
  */
 const char * lg_run_file(char *file_name) {
     printf("\nRunning %s...\n", file_name);
@@ -79,7 +75,11 @@ void lg_openlibs(void) {
     luaL_openlibs(_L);
 }
 /*
- *
+ * Load a custom module
+ * module_func: a pointer to a function that sets up the custom module.
+ * Returns: nothing.
+ * The function's signature must be:
+ *      void function_name(lua_State *L)
  */
 void lg_load_module(void (*module_func)(lua_State *)) {
     printf("Loading module at address %p\n", module_func);
